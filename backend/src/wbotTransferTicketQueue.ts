62fced2046b9c69397f0b5d8cf5d373c33ea092b
Cristianoiai <cristiano@ipbx.com.br>
2024-11-07 12:07:12 -0300

    teste de commit

--- backend/src/wbotTransferTicketQueue.ts
+++ backend/src/wbotTransferTicketQueue.ts
@@ -13,7 +13,7 @@ export const TransferTicketQueue = async (): Promise<void> => {
 
   const io = getIO();
 
-  //buscar os tickets que em pendentes e sem fila
+  // buscar os tickets que em pendentes e sem fila
   const tickets = await Ticket.findAll({
     where: {
       status: "pending",
